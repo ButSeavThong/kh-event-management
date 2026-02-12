066ef69f9489fa85967ebc550a395c02fd2b4cba
ButSeavThong <rope61963@gmail.com>
2026-02-12 22:38:17 +0700

    Allow vercel origin

--- src/main/java/com/thong/event/utils/CorsConfig.java
+++ src/main/java/com/thong/event/utils/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.thong.event.utils;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://event-management-seven-woad.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
