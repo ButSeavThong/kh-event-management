171fe2f8a8afb23a2d2cea05940751000b144b4a
ButSeavThong <rope61963@gmail.com>
2026-02-13 20:56:53 +0700

    image url fixed

--- src/main/java/com/thong/event/feature/user/dto/UpdateProfileRequest.java
+++ src/main/java/com/thong/event/feature/user/dto/UpdateProfileRequest.java
@@ -1,8 +1,11 @@
 package com.thong.event.feature.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UpdateProfileRequest(
         String username,
         String email,
+        @JsonProperty("profileImage")
         String profileImage
 ) {
 }
